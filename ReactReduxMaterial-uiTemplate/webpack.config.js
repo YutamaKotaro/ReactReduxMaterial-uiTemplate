b9aacffc586592019516da9b10c520dbad43fac8
YutamaKotaro <california.dreamin.never.die@gmail.com>
2016-08-25 15:35:17 +0900

    firstCommit

---
+++ ReactReduxMaterial-uiTemplate/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry:'./src/App',
+    output:{
+        filename:'./build/bundle.js'
+    },
+    module:{
+        loaders:[{
+            test: /\.js$/,
+            loader:'babel',
+            exclude:[/node_modules/],
+            query:{
+                presets:['es2015','stage-1','react'],
+                plugins:[
+                  'transform-flow-strip-types',
+                  'transform-class-properties',
+                  'transform-decorators-legacy'
+                ]
+            }
+        }]
+    }
+};
